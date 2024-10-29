bf1b6a5960dc9e865e22febf48f89b9770417009
Dan Ash <danash001@gmail.com>
2024-10-29 22:54:59 +0100

    modify icon files position and remove the fingerprint for the pngs

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'img/icons/*.png'],
+      includeAssets: ['favicon.ico', '*.png'],
       manifest: {
         name: 'Password Generator',
         short_name: 'PassGen',
@@ -19,42 +19,42 @@ export default defineConfig({
         scope: '/genpass-ui/',
         icons: [
           {
-            src: 'img/icons/icon-72x72.png',
+            src: 'icon-72x72.png',
             sizes: '72x72',
             type: 'image/png'
           },
           {
-            src: 'img/icons/icon-96x96.png',
+            src: 'icon-96x96.png',
             sizes: '96x96',
             type: 'image/png'
           },
           {
-            src: 'img/icons/icon-128x128.png',
+            src: 'icon-128x128.png',
             sizes: '128x128',
             type: 'image/png'
           },
           {
-            src: 'img/icons/icon-144x144.png',
+            src: 'icon-144x144.png',
             sizes: '144x144',
             type: 'image/png'
           },
           {
-            src: 'img/icons/icon-152x152.png',
+            src: 'icon-152x152.png',
             sizes: '152x152',
             type: 'image/png'
           },
           {
-            src: 'img/icons/icon-192x192.png',
+            src: 'icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'img/icons/icon-384x384.png',
+            src: 'icon-384x384.png',
             sizes: '384x384',
             type: 'image/png'
           },
           {
-            src: 'img/icons/icon-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
@@ -64,7 +64,8 @@ export default defineConfig({
         cleanupOutdatedCaches: true,
         skipWaiting: true,
         clientsClaim: true,
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        globIgnores: ['**/node_modules/**/*']
       },
       injectRegister: 'auto'
     })
@@ -77,5 +78,17 @@ export default defineConfig({
   base: '/genpass-ui/',  // Replace with your actual repo name
   build: {
     outDir: 'dist',  // Explicitly set the output directory
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Don't add hash to png files
+          if (assetInfo.name.endsWith('.png')) {
+            return 'assets/[name][extname]';
+          }
+          // Use hash for other assets
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
   }
 }) 
